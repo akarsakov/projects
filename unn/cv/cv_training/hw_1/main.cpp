737099a79e03c14f79e35b7bbf7d5daea60f547c
Alexander Karsakov <alexander.karsakov@itseez.com>
2014-04-29 12:09:30 +0400

    Calculation with integral image

--- unn/cv/cv_training/hw_1/main.cpp
+++ unn/cv/cv_training/hw_1/main.cpp
@@ -7,10 +7,8 @@ using namespace cv;
 
 int main()
 {
+    double k = 0.2;
     Mat image = imread("lena.png");
-    namedWindow( "Source image", WINDOW_AUTOSIZE );
-    imshow( "Source image", image );
-    waitKey(0);
     
     // convert to gray
     Mat gray_image;
@@ -22,38 +20,47 @@ int main()
 
     // canny
     Mat edges_image;
-    Canny(gray_image, edges_image, 100, 150, 3);
+    Canny(gray_image, edges_image, 150, 175, 3);
     namedWindow( "After Canny filter", WINDOW_AUTOSIZE );
     imshow( "After Canny filter", edges_image );
     waitKey(0);
 
+    // invert binary image
+    threshold(edges_image, edges_image, 125, 255, THRESH_BINARY_INV);
+
     // calc distance transform
     Mat distance_image;
-    distanceTransform(edges_image, distance_image, 2, 5);
+    distanceTransform(edges_image, distance_image, CV_DIST_L2, 5);
+    distance_image.convertTo(distance_image, CV_32S, 1, 0.5);
 
     // calc integral image
     Mat integral_image;
-    integral(image, integral_image);
+    integral(gray_image, integral_image);
 
     Mat result(image.size(), CV_8U);
     for (int i=0; i<image.rows; i++)
     {
         for (int j=0; j<image.cols; j++)
         {
-            int aperture_size = distance_image.at<int>(i,j);
-            int new_val = 0;
-            for (int y=0; y<aperture_size; y++)
+            int aperture_size = k*distance_image.at<int>(i,j);
+            if (aperture_size == 0)
             {
-                int y_coord = borderInterpolate(y - (aperture_size + 1)/2, image.rows, BORDER_REFLECT_101);
-                for (int x=0; x<aperture_size; x++)
-                {
-                    int x_coord = borderInterpolate(x - (aperture_size + 1)/2, image.cols, BORDER_REFLECT_101);
-                }
+                result.at<uchar>(i,j) = gray_image.at<uchar>(i,j);
+            } else
+            {
+                int x_left = max(j - (aperture_size + 1)/2, 0);
+                int x_right = min(j + (aperture_size + 1)/2, image.cols - 1);
+                int y_up = max(i - (aperture_size + 1)/2, 0);
+                int y_down = min(i + (aperture_size + 1)/2, image.rows - 1);
+                int s = integral_image.at<int>(y_up, x_left) + integral_image.at<int>(y_down, x_right) - integral_image.at<int>(y_down, x_left) - integral_image.at<int>(y_up, x_right);
+                int new_val = s / ((x_right - x_left)*(y_down - y_up));
+                new_val &= 255;
+                result.at<uchar>(i,j) = new_val;
             }
         }
     }
-
-    
-
+    namedWindow( "After mean filetring", WINDOW_AUTOSIZE );
+    imshow( "After mean filetring", result );
+    waitKey(0);
     return 0;
 };
